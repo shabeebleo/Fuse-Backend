7e8fcb49ee84291f6d049c5cd8621e08da95761a
shabeebleo <shabeebpv0786@gmail.com>
2023-01-17 11:29:46 +0530

    cors origin change

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({origin:'http://localhost:3000'}))
+app.use(cors({origin:'*'}))
 dotenv.config();
 
 mongoose
