ab72e7939fb129558fcf0cf62cab3deba07a7ed5
shabeebleo <shabeebpv0786@gmail.com>
2023-01-18 19:06:58 +0530

    socket added

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ import MessageRoute from "./Routes/MessageRoute.js"
 import AdminRoute from "./Routes/AdminRoute.js"
 import {errorHandler} from "./middlewares/errorMiddlewares.js";
 import cors from 'cors'
+import {Server} from 'socket.io'
+// import { createServer }  from 'http'
 //route
 const app = express();
+
 
 //middleware
 
@@ -20,6 +23,65 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 dotenv.config();
+
+
+
+// const httpServer = createServer(app);
+
+//SOCKET PART
+const io = new Server(8800, {
+  cors: {
+      origin: ["http://localhost:3000", "https://master.d324cc897mcqpi.amplifyapp.com"],
+  },
+});
+
+let activeUsers = [];
+
+io.on("connection", (socket) => {
+  // add new User
+  socket.on("new-user-add", (newUserId) => {
+    // if user is not added previously
+    if(newUserId!=null){
+      if (!activeUsers.some((user) => user.userId == newUserId)) {
+        activeUsers.push({ userId: newUserId, socketId: socket.id });
+        console.log("New User Connected", activeUsers);
+      }
+      // send all active users to new user
+      io.emit("get-users", activeUsers);
+    }
+    
+  });
+
+  socket.on("disconnect", () => {
+    // remove user from active users
+    activeUsers = activeUsers.filter((user) => user.socketId != socket.id);
+    console.log("User Disconnected", activeUsers);
+    // send all active users to all users
+    io.emit("get-users", activeUsers);
+  });
+
+  // send message to a specific user
+  socket.on("send-message", (data) => {
+    console.log(data,"data")
+    console.log(activeUsers,"active kusers in send messages")
+    const { receiverId } = data;
+    
+    const user = activeUsers.find((user) => {
+      return user.userId == receiverId
+    });
+    console.log("..............")
+    console.log(user,"user in send message")
+    console.log("Sending from socket to :", receiverId)
+    // console.log("Data: ", data)
+    if (user) {
+      io.to(user.socketId).emit("receive-message",data);
+    }
+  });
+})
+
+
+
+
 
 mongoose
   // eslint-disable-next-line
